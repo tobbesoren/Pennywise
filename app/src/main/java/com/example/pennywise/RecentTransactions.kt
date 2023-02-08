2c2482198d14d3e63145daa8f22b86a82731eaf0
Viktor <vjagebrant@gmail.com>
2023-02-08 20:17:16 +0100

    recentTransactionsActivity

---
+++ app/src/main/java/com/example/pennywise/RecentTransactions.kt
@@ -0,0 +1,11 @@
+package com.example.pennywise
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RecentTransactions : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_recent_transactions)
+    }
+}
